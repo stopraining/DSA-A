a2f8583455df64721f5449d122aa1df54a6d8aa0
stopraining <55649197+stopraining@users.noreply.github.com>
2019-10-03 10:28:13 +0800

    Update note.py

--- CodeSignal/note.py
+++ CodeSignal/note.py
@@ -1,4 +1,4 @@
-""bit count
+"""bit count
 計算
 50(十進位)=110010(二進位)
 計算:
@@ -7,9 +7,14 @@
   12÷2=6(餘0)  → 0
   6÷2=3(餘1)   → 0
   3÷2=1(餘1)   → 1
-  1÷2=0(餘1)   → 1 ""
+  1÷2=0(餘1)   → 1 """
 
-""baseCoversion:""
+"""baseCoversion"""
   def baseCoversion(n,x):
+    return hex(int(str(n),x))[2:]  """hex():從10進位→16進位"""
+ 
+"""Capitalize()
+   將首字母改大寫，其他小寫"""
+  
   
     
