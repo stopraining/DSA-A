e368d0a25f601af47a9d417184203bd4b790953d
stopraining <55649197+stopraining@users.noreply.github.com>
2019-10-06 11:34:50 +0800

    Update 707Design Linked List.py

--- LeetCode/707Design Linked List.py
+++ LeetCode/707Design Linked List.py
@@ -6,3 +6,21 @@ class MyLinkedList:
         """
         self.val = None
         self.next = None
+        
+    def get(self, index: int) -> int:
+        """
+        Get the value of the index-th node in the linked list. If the index is invalid, return -1.
+        """
+        if self.val == None :
+            return -1
+        
+        if index == 0:
+            return self.val  
+        p = self.next
+        i = 1
+        while p != None:
+            if i == index:
+                return p.val  
+            p = p.next       """p=self.next.next 下一個的下一個"""
+            i += 1        """i每次都在往上加"""
+        return -1
