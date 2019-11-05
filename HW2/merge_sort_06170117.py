b345e752b57b3ea6da46736638afc99914ca2a77
stopraining <55649197+stopraining@users.noreply.github.com>
2019-11-05 12:56:48 +0800

    Add files via upload

--- HW2/merge_sort_06170117.py
+++ HW2/merge_sort_06170117.py
@@ -1,46 +1,68 @@
-def merge_sort(list):
-    if len(list) <= 1:
-        return list
-    else:
-        left=[]
-        right=[]
-        n=0
-        for i in list:
-            list[n]=i
-            if n < (len(list)/2):
-                left.append(i)
-                n+=1
-                
-            elif n >= (len(list)/2):
-                right.append(i)
-                n+=1
-        left=merge_sort(left)
-        right=merge_sort(right)
-        return merge(list,left,right)
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[275]:
 
 
-def merge(list,left,right):
-    j=0
-    p=0
-    q=0
-    while p <len(left) and q<len(right):
-        if left[p] < right[q]:
-            list[j] =left[p]
-            p+=1
-               
-        elif left[p] >= right[q]:
-            list[j] =right[q]
-            q+=1
-        j+=1
+class Solution(object):
+    def merge_sort(self,data):
         
-    while p <len(left):
-        list[j] =left[p]
-        p+=1
-        j+=1
-    while  q<len(right):
-        list[j] =right[q]
-        q+=1
-        j+=1
+        if len(data) <= 1:
+            return data
+        else:
+            left=[]
+            right=[]
+            n=0
+            for i in data:
+                data[n]=i
+                if n < (len(data)/2):
+                    left.append(i)
+                    n+=1
+                    left=self.merge_sort(left)
+                elif n >= (len(data)/2):
+                    right.append(i)
+                    n+=1
+                    right=self.merge_sort(right)
+         
+            return self.merge(data,left,right)
+
+    def merge(self,data,left,right):
+   
+        j=0
+        p=0
+        q=0
+        
+        while p <len(left) and q<len(right):
+            if left[p] < right[q]:
+                data[j] =left[p]
+                p+=1
+               
+            elif left[p] >= right[q]:
+                data[j] =right[q]
+                q+=1
+            j+=1
+            
+        while p < len(left):
+            data[j] =left[p]
+            p+=1
+            j+=1
+        while q< len(right):
+            data[j] =right[q]
+            q+=1
+            j+=1     
+
+        return data
 
 
-    return list
+# In[274]:
+
+
+output=Solution().merge_sort([4,2,8,9,-12,-5])
+output
+
+
+# In[ ]:
+
+
+
+
