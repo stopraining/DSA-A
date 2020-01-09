96ed7dbf6b2979ada363b8cb5e57e3888cccc3c0
stopraining <55649197+stopraining@users.noreply.github.com>
2020-01-09 20:24:45 +0800

    Add files via upload

--- Leetcode/7_Reverse Integer_06170117.py
+++ Leetcode/7_Reverse Integer_06170117.py
@@ -1,22 +1,27 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[51]:
+# In[52]:
 
 
 class Solution:
     def reverse(self, x) :
-        x=str(x)
-        if x[0]=="-":
-            x=int('-' + x[-1:0:-1])  
+        if x < -2147483648 or x > 2147483648:
+            return 0
         else:
-            x =int(x[::-1])       
-                  
-        return x                  
-
-
-# In[ ]:
-
-
-
+            x=str(x)
+            if x[0]=="-":
+                x=int('-' + x[-1:0:-1])  
+                if x < -2147483648 or x > 2147483648:
+                    return 0
+                else:
+                    return x 
+            else:
+                x =int(x[::-1]) 
+                if x < -2147483648 or x > 2147483648:
+                    return 0
+                else:
+                    return x 
+        
+                   
 
